d23cbceba66c24265bca604ea405cc96bac0a698
prost333 <dimabolshoy333@yandex.by>
2024-01-05 21:54:07 +0300

    spring

--- src/main/java/by/clevertec/Entity/Entity.java
+++ src/main/java/by/clevertec/Entity/Entity.java
@@ -1,22 +1,7 @@
 package by.clevertec.Entity;
 
 
-import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
-import lombok.experimental.FieldNameConstants;
 
-
-import javax.validation.constraints.NotNull;
-import java.util.UUID;
-
-
-@Setter
-@Getter
-@Data
-@ToString
-@FieldNameConstants
 public class Entity {
 
 
